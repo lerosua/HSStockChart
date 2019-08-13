7f3839773bf7a75c8447de89e9e210ed1383eb17
lerosua <lerosua@gmail.com>
2019-08-13 16:40:34 +0800

    fix

--- HSStockChart/TimeLineChart/HSTimeLineStyle.swift
+++ HSStockChart/TimeLineChart/HSTimeLineStyle.swift
@@ -56,17 +56,17 @@ extension HSTimeLineStyle{
              avgLineColor = UIColor.hschart.color(rgba: "#FD8A00") // 均线颜色
              fillColor = UIColor.hschart.color(rgba: "#262F47")
         }else{
-            ma5Color = UIColor.hschart.color(rgba: "#e8de85")
-            ma10Color = UIColor.hschart.color(rgba: "#6fa8bb")
-            ma20Color = UIColor.hschart.color(rgba: "#df8fc6")
-            borderColor = UIColor.hschart.color(rgba: "#e4e4e4")
-            crossLineColor = UIColor.hschart.color(rgba: "#546679")
-            textColor = UIColor.hschart.color(rgba: "#8695a6")
-            riseColor = UIColor.hschart.color(rgba: "#f24957") // 涨 red
-            fallColor = UIColor.hschart.color(rgba: "#1dbf60") // 跌 green
-            priceLineColor = UIColor.hschart.color(rgba: "#0095ff")
-            avgLineColor = UIColor.hschart.color(rgba: "#ffc004") // 均线颜色
-            fillColor = UIColor.hschart.color(rgba: "#e3efff")
+             ma5Color = UIColor(red: 134/255, green: 149/255, blue: 166/255, alpha: 1)
+             ma10Color = UIColor(red: 111/255, green: 168/255, blue: 167/255, alpha: 1)
+             ma20Color = UIColor(red: 111/255, green: 168/255, blue: 167/255, alpha: 1)
+             borderColor = UIColor(red: 228/255, green: 228/255, blue: 228/255, alpha: 1)
+             crossLineColor = UIColor(red: 84/255, green: 102/255, blue: 121/255, alpha: 1)
+             textColor = UIColor(red: 134/255, green: 149/255, blue: 166/255, alpha: 1)
+             riseColor = UIColor(red: 242/255, green: 73/255, blue: 87/255, alpha: 1) // 涨 red
+             fallColor = UIColor(red: 29/255, green: 191/255, blue: 96/255, alpha: 1) // 跌 green
+             priceLineColor = UIColor(red: 0/255, green: 149/255, blue: 252/255, alpha: 1)
+             avgLineColor = UIColor(red: 255/255, green: 192/255, blue: 4/255, alpha: 1)
+             fillColor = UIColor(red: 227/255, green: 239/255, blue: 255/255, alpha: 1)
         }
     }
 }
